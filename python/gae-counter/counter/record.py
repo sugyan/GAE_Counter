3080aecfc5634fd5554156db01f2b28e214fad87
sugyan <sugi1982@gmail.com>
2009-05-04 23:26:21 +0900

    アクセス履歴をとれるよう機能追加

--- python/gae-counter/counter/record.py
+++ python/gae-counter/counter/record.py
@@ -11,39 +11,32 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import login_required
 from common import responses
 from models.counter import Counter
-from models.image import NumberImage
 from models.record import AccessRecord
 
 
-class DestroyHandler(webapp.RequestHandler):
+class RecordHandler(webapp.RequestHandler):
     """
-    カウンターを削除する
+    カウンターのアクセス記録を表示する
     """
-
+    
+    @login_required
     def get(self):
-        """
-        GETは404とする
-        """
-        responses.display_error(self, 404)
-
-    def post(self):
-        """
-        requestからkeyを受け取り、対応するCounterのentityを削除する
-        """
         try:
-            # 削除する対象を取得
+            user = users.get_current_user()
+            # requestのkeyに対応するCounterを取得
             counter = Counter.get(db.Key(encoded = self.request.get('key')))
             # 現在のユーザーと関連づけられていなければ403エラー
-            if counter == None or counter.user != users.get_current_user():
+            if counter == None or user != counter.user:
                 responses.display_error(self, 403)
                 return
-            # 関連画像、アクセス履歴ともに一括削除
-            for image in counter.image:
-                NumberImage.get(image).delete()
-            for record in AccessRecord.all().ancestor(counter):
-                record.delete()
-            counter.delete()
-            self.redirect('/')
+            template_values = {
+                'user'     : user,
+                'user_url' : users.create_logout_url('/'),
+                'counter'  : counter,
+                'records'  : AccessRecord.all().ancestor(counter),
+                }
+            path = os.path.join(os.path.dirname(__file__), os.pardir, 'templates', 'record.html')
+            self.response.out.write(template.render(path, template_values))
         # requestのkeyが正しくない場合
         except BadKeyError, error:
             logging.error(str(error))
